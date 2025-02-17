18a1e46a707b051d6098f768f6771cd48ea64f88
chinnhong.seng <chinnhong.rule@gmail.com>
2025-02-17 10:33:21 +0700

    update

--- app/src/main/java/com/example/testgeneratedoc/MyApplication.kt
+++ app/src/main/java/com/example/testgeneratedoc/MyApplication.kt
@@ -2,20 +2,20 @@ package com.example.testgeneratedoc
 
 import android.app.Application
 import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
+//import com.facebook.flipper.android.utils.FlipperUtils
+//import com.facebook.flipper.plugins.inspector.DescriptorMapping
+//import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+//import com.facebook.soloader.SoLoader
 
 class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
-
-        if (FlipperUtils.shouldEnableFlipper(this)) {
-            val client = AndroidFlipperClient.getInstance(this)
-            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
-            client.start()
-        }
+//        SoLoader.init(this, false)
+//
+//        if (FlipperUtils.shouldEnableFlipper(this)) {
+//            val client = AndroidFlipperClient.getInstance(this)
+//            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+//            client.start()
+//        }
     }
 }
